d65f4466239a49981a7a1e080f527697f3e9b810
所念皆星河 <1357513467@qq.com>
2024-04-23 17:59:18 +0800

    第二天

---
+++ 数组：4.长度最小的子数组.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int minSubArrayLen(int target, vector<int>& nums) {
+//    int i,j,sum,result;
+//    //result要设一个很大的值
+//    i=0; sum=0; result=INT32_MAX;
+//    for(j=0;j<nums.size();j++)
+//    {
+//        sum+=nums[j];
+//        while(sum>=target)
+//        {
+//            int subL=j-i+1;
+//            result=min(subL,result);
+//            sum-=nums[i];
+//            i++;
+//        }
+//    }
+//    //如果result没有更新，就返回0
+//    return result==INT32_MAX?0:result;
+//}
+//
+//int main()
+//{
+//
+//    vector<int>nums={2,3,1,2,4,3};
+//    int target=7;
+//    cout<<minSubArrayLen(target,nums);
+//
+//    return 0;
+//}
---
+++ 数组：5.螺旋矩阵II.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>nums(n,vector<int>(n,0));
+        int startX=0;
+        int startY=0;
+        int offset=1;
+        int count=1;
+        int loop=n/2;
+        int i,j;
+        i=0; j=0;
+        while(loop--)
+        {
+            j=startY;
+            i=startX;
+            for(j;j<n-offset;j++)
+            {
+                nums[i][j]= count;
+                count++;
+            }
+            for(i;i<n-offset;i++)
+            {
+                nums[i][j]=count;
+                count++;
+            }
+            for(;j>startY;j--)
+            {
+                nums[i][j]=count;
+                count++;
+            }
+            for(;i>startX;i--)
+            {
+                nums[i][j]=count;
+                count++;
+            }
+            startX++;
+            startY++;
+
+            offset+=1;
+        }
+        if(n%2!=0)
+        {
+            nums[n/2][n/2]=count;
+        }
+        return nums;
+    }
+
+int main()
+{
+    int n=1;
+
+    for(int i=0;i<generateMatrix(n).size();i++)
+    {
+        for(int j=0;j<generateMatrix(n)[0].size();j++)
+        {
+            cout<<generateMatrix(n)[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}

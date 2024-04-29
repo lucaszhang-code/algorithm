f9f70b58b52c209e8d474291239895eae1d6fa76
lucaszhang-code <1357513467@qq.com>
2024-04-29 20:00:34 +0800

    第七天

---
+++ 哈希表：3.两数之和.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <unordered_map>
+//
+//vector<int> twoSum(vector<int>& nums, int target) {
+//    unordered_map<int,int> map;
+//    for(int i=0;i<nums.size();i++){
+//        int s=target-nums[i];
+//        auto iter=map.find(s);
+//        if(iter!=map.end())
+//            return {iter->second,i};
+//        map.insert(pair<int,int>(nums[i],i));
+//    }
+//    return{};
+//}
+//
+//int main(){
+//
+//    vector<int>nums {2,7,11,15};
+//    int target=18;
+//    cout<<twoSum(nums,target).at(0)<<" "<<twoSum(nums,target).at(1)<<endl;
+//
+//
+//    return 0;
+//}
